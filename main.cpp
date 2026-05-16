324e051947d138de06ba922fe85c46098cd686d9
Fedor Zhuk <bittlef1998@gmail.com>
2026-05-16 05:41:00 +0200

    added icon

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main()
 	unsigned int x_size = BOARD_SIZE * TILE_SIZE + (BOARD_SIZE * TILE_SIZE * 0.25);
 	unsigned int y_size = BOARD_SIZE * TILE_SIZE + (BOARD_SIZE * TILE_SIZE * 0.03);
 	sf::RenderWindow window(sf::VideoMode({x_size, y_size}), "Checkers");
+	sf::Image icon;
+	icon.loadFromFile("img/icon.png");
+	window.setIcon({icon.getSize().x, icon.getSize().y}, icon.getPixelsPtr());
+
     Checkers game;
 
 	while ( window.isOpen() )
